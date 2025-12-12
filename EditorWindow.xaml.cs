bf54f64d642eda96a6657741545be760e66a1238
ZonBock1337 <zonigaming1337@gmail.com>
2025-12-12 16:26:40 +0100

    Initial commit of Shimeji-ee Playground

---
+++ EditorWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
+using System.Windows.Threading;
+
+namespace ShimejiPlaygroundApp
+{
+    [Serializable]
+    public class EditorSettings
+    {
+        public string WindowTitle = "Shimeji Playground";
+        public double WindowWidth = 960;
+        public double WindowHeight = 540;
+        public string BackgroundPath = "assets/background.png";
+        public bool StartDirectPlayground = false;
+    }
+
+    public partial class EditorWindow : Window
+    {
+        private PlaygroundWindow playground;
+        private string settingsFile = "editor_settings.xml";
+        private EditorSettings settings;
+
+        public EditorWindow()
+        {
+            InitializeComponent();
+            LoadSettings();
+            ApplySettingsToUI();
+
+            InputBindings.Add(new KeyBinding(
+                new RelayCommand(() => ReturnFromPlayground()),
+                new KeyGesture(Key.S, ModifierKeys.Control)));
+
+            if (settings.StartDirectPlayground)
+                Dispatcher.BeginInvoke(new Action(LaunchPlayground), DispatcherPriority.ApplicationIdle);
+        }
+
+        private void LoadSettings()
+        {
+            if (File.Exists(settingsFile))
+            {
+                try
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(EditorSettings));
+                    using (var stream = File.OpenRead(settingsFile))
+                        settings = (EditorSettings)serializer.Deserialize(stream);
+                }
+                catch { settings = new EditorSettings(); }
+            }
+            else settings = new EditorSettings();
+        }
+
+        private void SaveSettings()
+        {
+            settings.WindowTitle = WindowTitleTextBox.Text;
+            settings.WindowWidth = double.TryParse(WidthTextBox.Text, out double w) ? w : 500;
+            settings.WindowHeight = double.TryParse(HeightTextBox.Text, out double h) ? h : 500;
+            settings.BackgroundPath = SelectedImageText.Text;
+            settings.StartDirectPlayground = StartDirectPlaygroundCheckBox.IsChecked ?? false;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(EditorSettings));
+            using (var stream = File.Create(settingsFile))
+                serializer.Serialize(stream, settings);
+        }
+
+        private void ApplySettingsToUI()
+        {
+            WindowTitleTextBox.Text = settings.WindowTitle;
+            WidthTextBox.Text = settings.WindowWidth.ToString();
+            HeightTextBox.Text = settings.WindowHeight.ToString();
+            SelectedImageText.Text = settings.BackgroundPath;
+            StartDirectPlaygroundCheckBox.IsChecked = settings.StartDirectPlayground;
+
+            if (File.Exists(settings.BackgroundPath))
+                PreviewImage.Source = new BitmapImage(new Uri(settings.BackgroundPath, UriKind.RelativeOrAbsolute));
+        }
+
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp;*.gif";
+            if (dlg.ShowDialog() == true)
+            {
+                SelectedImageText.Text = dlg.FileName;
+                PreviewImage.Source = new BitmapImage(new Uri(dlg.FileName, UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void ResetDefaults_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to reset all settings?", "Confirm Reset", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                settings = new EditorSettings();
+                ApplySettingsToUI();
+            }
+        }
+
+        private void RunButton_Click(object sender, RoutedEventArgs e) => LaunchPlayground();
+
+        private void LaunchPlayground()
+        {
+            SaveSettings();
+            playground = new PlaygroundWindow(settings, () =>
+            {
+                this.Show();
+            });
+            playground.Show();
+            this.Hide();
+        }
+
+        private void ReturnFromPlayground()
+        {
+            if (playground != null)
+            {
+                playground.Close();
+                this.Show();
+            }
+        }
+
+        private void EditorWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.S && Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
+                ReturnFromPlayground();
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action action;
+        public RelayCommand(Action a) { action = a; }
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => action();
+    }
+}
---
+++ PlaygroundWindow.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Windows.Input;
+
+namespace ShimejiPlaygroundApp
+{
+    public class PlaygroundWindow : Window
+    {
+        private EditorSettings settings;
+        private DispatcherTimer checkPositionTimer;
+        private DateTime lastCheckTime;
+        private Action returnToEditorAction;
+
+        private const double OutOfScreenMargin = 20; // Puffer for out-of-screen
+
+        public PlaygroundWindow(EditorSettings settings, Action returnToEditor)
+        {
+            this.settings = settings;
+            this.returnToEditorAction = returnToEditor;
+            InitializeWindow();
+            StartPositionCheckTimer();
+        }
+
+        private void InitializeWindow()
+        {
+            this.Title = settings.WindowTitle;
+            this.Width = settings.WindowWidth;
+            this.Height = settings.WindowHeight;
+            this.WindowStyle = WindowStyle.None;
+            this.ResizeMode = ResizeMode.CanResize;
+            this.AllowsTransparency = true;
+            this.Background = System.Windows.Media.Brushes.Transparent;
+            this.Topmost = false;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            Grid grid = new Grid();
+
+            if (System.IO.File.Exists(settings.BackgroundPath))
+            {
+                Image bg = new Image
+                {
+                    Source = new BitmapImage(new Uri(settings.BackgroundPath, UriKind.RelativeOrAbsolute)),
+                    Stretch = System.Windows.Media.Stretch.Fill
+                };
+                grid.Children.Add(bg);
+            }
+
+            Button closeButton = new Button
+            {
+                Content = "X",
+                Width = 30,
+                Height = 30,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(5)
+            };
+            closeButton.Click += (s, e) => this.Close();
+            grid.Children.Add(closeButton);
+
+            this.Content = grid;
+
+            this.Opacity = 0;
+            this.Loaded += (s, e) =>
+            {
+                var anim = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(0.5));
+                this.BeginAnimation(Window.OpacityProperty, anim);
+            };
+
+            this.MouseLeftButtonDown += (s, e) =>
+            {
+                if (e.ButtonState == MouseButtonState.Pressed)
+                    this.DragMove();
+            };
+
+            this.KeyDown += PlaygroundWindow_KeyDown;
+
+            this.Closing += (s, e) =>
+            {
+                checkPositionTimer?.Stop();
+            };
+
+            lastCheckTime = DateTime.Now;
+        }
+
+        private void PlaygroundWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.S && (Keyboard.Modifiers & ModifierKeys.Shift) != 0)
+            {
+                returnToEditorAction?.Invoke();
+                this.Close();
+            }
+        }
+
+        private void StartPositionCheckTimer()
+        {
+            checkPositionTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+            checkPositionTimer.Tick += (s, e) =>
+            {
+                var elapsed = (DateTime.Now - lastCheckTime).TotalSeconds;
+                if (elapsed >= 10)
+                    CheckWindowPosition();
+            };
+            checkPositionTimer.Start();
+        }
+
+        private void CheckWindowPosition()
+        {
+            var screen = SystemParameters.WorkArea;
+
+            bool outOfScreen =
+                Left + Width < 0 || Top + Height < 0 ||
+                Left > screen.Right || Top > screen.Bottom;
+
+            if (!outOfScreen) return;
+
+            double targetLeft = (screen.Width - Width) / 2;
+            double targetTop = (screen.Height - Height) / 2;
+
+            var animLeft = new DoubleAnimation
+            {
+                From = Left,
+                To = targetLeft,
+                Duration = TimeSpan.FromSeconds(2.5),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut },
+                FillBehavior = FillBehavior.Stop
+            };
+            animLeft.Completed += (s, e) => Left = targetLeft;
+
+            var animTop = new DoubleAnimation
+            {
+                From = Top,
+                To = targetTop,
+                Duration = TimeSpan.FromSeconds(2.5),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut },
+                FillBehavior = FillBehavior.Stop
+            };
+            animTop.Completed += (s, e) => Top = targetTop;
+
+            this.BeginAnimation(Window.LeftProperty, animLeft);
+            this.BeginAnimation(Window.TopProperty, animTop);
+
+            lastCheckTime = DateTime.Now;
+        }
+    }
+}
